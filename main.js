bbbf82c99715af14009d8267720680c2473ec865
andrea-1987 <142450665+andrea-1987@users.noreply.github.com>
2024-04-05 10:29:44 +0200

    onwork

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ const path = require("path");
 const booksRoutes = require("./routes/books");
 const usersRoutes = require("./routes/users");
 const blogsRoutes = require("./routes/blogs");
-const loginRoutes = require("./routes/login")
+const loginRoutes = require("./routes/login");
 const connectToDataBase = require("./db");
-const cors= require("cors")
+const cors = require("cors");
 
 const PORT = 3030;
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 // app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
